f5675b0c493e5d8d9b667518a3388dfbec0c4e86
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2024-12-05 16:05:57 +0530

    Settings screen Tablet  UI

--- app/src/main/java/com/example/timecapsule/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/example/timecapsule/ui/setting/SettingScreen.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -24,16 +26,23 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.timecapsule.ui.theme.LightBlue
+import com.example.timecapsule.ui.util.DeviceType
 
 @Preview
 @Composable
 fun SettingScreen() {
+
+  val context = LocalContext.current
+
+  val isTablet = DeviceType.isTablet()
+
   Scaffold() { innerPadding ->
     Box(
       modifier = Modifier
@@ -43,10 +52,16 @@ fun SettingScreen() {
     ) {
       LazyColumn(
         modifier = Modifier
-          .fillMaxSize()
+          .fillMaxHeight()
+          .then(
+            if (isTablet)
+              Modifier.width(600.dp)
+            else
+              Modifier.fillMaxWidth()
+          )
           .padding(top = 30.dp)
           .background(MaterialTheme.colorScheme.primary)
-          .align(Alignment.TopStart)
+          .align(Alignment.TopCenter)
           .background(Color.Transparent)
       ) {
         item {
@@ -85,7 +100,7 @@ fun SettingScreen() {
 
             com.example.timecapsule.ui.setting.Divider(color = Color.Gray)
 
-            SettingOptionsTab(settingTitle = "Rate out app") {
+            SettingOptionsTab(settingTitle = "Rate our app") {
             }
 
             com.example.timecapsule.ui.setting.Divider(color = Color.Gray)
