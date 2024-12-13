c344ce095048ed2c11fdc366c2c7ca54eebae967
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2024-12-13 16:46:50 +0530

    Redesign capsule list  Screen

--- app/src/main/java/com/example/timecapsule/ui/capsulelist/v2/CapsuleCardListScreen.kt
+++ app/src/main/java/com/example/timecapsule/ui/capsulelist/v2/CapsuleCardListScreen.kt
@@ -2,6 +2,10 @@ package com.example.timecapsule.ui.capsulelist.v2
 
 import android.util.Log
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -33,15 +37,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -50,6 +58,7 @@ import coil.compose.AsyncImage
 import com.example.timecapsule.R
 import com.example.timecapsule.ui.theme.LightBlue
 import com.example.timecapsule.ui.theme.openSansExtraBold
+import kotlinx.coroutines.launch
 
 enum class Filter {
   ALL,
@@ -63,10 +72,26 @@ enum class Filter {
 )
 @Composable
 fun CapsuleCardListScreen() {
-  val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+  val rotation = remember { Animatable(0f) }
+  val coroutineScope = rememberCoroutineScope()
 
   var filter by remember {
     mutableStateOf(Filter.ALL)
+  }
+
+  // Trigger the animation
+  LaunchedEffect(filter) {
+    rotation.snapTo(0f)
+    coroutineScope.launch {
+      rotation.animateTo(
+        targetValue = 360f * 500F,
+        animationSpec = tween(
+          durationMillis = 2000,
+          easing = LinearOutSlowInEasing
+        )
+      )
+    }
   }
 
   Scaffold(
@@ -88,8 +113,8 @@ fun CapsuleCardListScreen() {
 
     Column(
       modifier = Modifier
-          .background(MaterialTheme.colorScheme.primary)
-          .padding(innerPadding)
+        .background(MaterialTheme.colorScheme.primary)
+        .padding(innerPadding)
 
     ) {
 
@@ -97,25 +122,24 @@ fun CapsuleCardListScreen() {
 
       LazyColumn(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 3.dp),
+          .fillMaxSize()
+          .padding(horizontal = 3.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
         contentPadding = PaddingValues(0.dp),
       ) {
         item {
           Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(100.dp)
-                .padding(horizontal = 10.dp),
+              .fillMaxWidth()
+              .height(100.dp)
+              .padding(start = 12.dp, end = 17.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
           ) {
-
             Text(
               "Time Capsule",
               style = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 29.sp,
+                fontSize = 30.sp,
                 fontWeight = FontWeight.ExtraBold,
                 fontFamily = openSansExtraBold
               )
@@ -124,9 +148,11 @@ fun CapsuleCardListScreen() {
               model = R.drawable.onboarding_image,
               contentDescription = "Profile Picture",
               modifier = Modifier
-                  .size(70.dp)
-                  .clip(CircleShape)
-                  .padding(end = 1.dp)
+                .size(50.dp)
+                .clip(CircleShape)
+                .padding(end = 1.dp)
+                .graphicsLayer(rotationZ = rotation.value),
+              contentScale = ContentScale.Crop
             )
           }
         }
@@ -134,9 +160,9 @@ fun CapsuleCardListScreen() {
         item {
           Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-                .padding(horizontal = 10.dp),
+              .fillMaxWidth()
+              .height(50.dp)
+              .padding(horizontal = 10.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start,
           ) {
@@ -145,9 +171,9 @@ fun CapsuleCardListScreen() {
               colors = ChipDefaults.chipColors(backgroundColor = LightBlue),
               onClick = { filter = Filter.ALL },
               modifier = Modifier
-                  .padding(horizontal = 3.dp)
-                  .height(40.dp)
-                  .animateContentSize()
+                .padding(horizontal = 3.dp)
+                .height(40.dp)
+                .animateContentSize()
             ) {
               Icon(
                 modifier = Modifier.padding(end = 3.dp),
@@ -166,9 +192,9 @@ fun CapsuleCardListScreen() {
               colors = ChipDefaults.chipColors(backgroundColor = LightBlue),
               onClick = { filter = Filter.ACTIVE },
               modifier = Modifier
-                  .padding(horizontal = 3.dp)
-                  .height(40.dp)
-                  .animateContentSize()
+                .padding(horizontal = 3.dp)
+                .height(40.dp)
+                .animateContentSize()
             ) {
               Icon(
                 modifier = Modifier.padding(end = 3.dp),
@@ -190,9 +216,9 @@ fun CapsuleCardListScreen() {
               colors = ChipDefaults.chipColors(backgroundColor = LightBlue),
               onClick = { filter = Filter.OPENED },
               modifier = Modifier
-                  .padding(horizontal = 3.dp)
-                  .height(40.dp)
-                  .animateContentSize()
+                .padding(horizontal = 3.dp)
+                .height(40.dp)
+                .animateContentSize()
             ) {
               Icon(
                 modifier = Modifier.padding(end = 3.dp),
@@ -228,11 +254,11 @@ fun CapsuleCardListScreen() {
               } else
                 Box(
                   modifier = Modifier
-                      .weight(
-                          if (isExpanded) 1f else 0.5f,
-                          fill = false
-                      )
-                      .animateContentSize()
+                    .weight(
+                      if (isExpanded) 1f else 0.5f,
+                      fill = false
+                    )
+                    .animateContentSize()
                 ) {
                   CapsuleCard(
                     isExpanded = isExpanded,
@@ -247,8 +273,8 @@ fun CapsuleCardListScreen() {
             if (rowItems[0] != expandedCardIndex)
               Box(
                 modifier = Modifier
-                    .fillMaxWidth(0.5F)
-                    .padding(top = 8.dp)
+                  .fillMaxWidth(0.5F)
+                  .padding(top = 8.dp)
               ) {
                 CapsuleCard(
                   isExpanded = expandedCardIndex == rowItems[0],
@@ -260,8 +286,8 @@ fun CapsuleCardListScreen() {
             else
               Box(
                 modifier = Modifier
-                    .fillMaxWidth(0.5F)
-                    .padding(top = 8.dp)
+                  .fillMaxWidth(0.5F)
+                  .padding(top = 8.dp)
               ) {
                 CapsuleCard(
                   isExpanded = expandedCardIndex == rowItems[1],
