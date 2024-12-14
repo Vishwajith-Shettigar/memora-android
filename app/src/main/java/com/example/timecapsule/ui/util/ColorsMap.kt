2d34d02d5176597af4c0217b573764224e129871
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2024-12-14 11:20:17 +0530

    Added new card color

--- app/src/main/java/com/example/timecapsule/ui/util/ColorsMap.kt
+++ app/src/main/java/com/example/timecapsule/ui/util/ColorsMap.kt
@@ -2,13 +2,14 @@ package com.example.timecapsule.ui.util
 
 import androidx.compose.ui.graphics.Color
 import com.example.timecapsule.ui.theme.cardGreen
+import com.example.timecapsule.ui.theme.cardOrange
 import com.example.timecapsule.ui.theme.cardViolet
 import com.example.timecapsule.ui.theme.cardYellow
 
 object ColorsMap {
   val colors: List<Color> = listOf(
     cardGreen,
-    cardViolet, cardYellow
+    cardViolet, cardYellow, cardOrange
   )
 
   fun getColor(index: Int): Color {
